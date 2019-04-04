1beb13127cc2d1ab0a87980f51f5ace305d73d6c
Kyle Sellers <kselle6@lsu.edu>
2019-04-04 16:19:24 +0200

    Create Sound and Instrument classes Attempt fix at looping

---
+++ app/src/main/java/com/example/miniproject/Drums.java
@@ -0,0 +1,28 @@
+package com.example.miniproject;
+
+import android.media.MediaPlayer;
+
+import com.livelife.motolibrary.AntData;
+
+import java.util.ArrayList;
+
+public class Drums {
+    private ArrayList<Sound> drumSounds;
+
+    public Drums() {
+        drumSounds = new ArrayList<>();
+        Sound kick = new Sound(R.raw.kick707, AntData.LED_COLOR_BLUE, "Kick");
+        Sound snare = new Sound(R.raw.snare707, AntData.LED_COLOR_RED, "Snare");
+        Sound closedHat = new Sound(R.raw.closedhat707, AntData.LED_COLOR_WHITE, "Closed Hi-Hat");
+        Sound openHat = new Sound(R.raw.openhat707, AntData.LED_COLOR_ORANGE, "Open Hi-Hat");
+
+        drumSounds.add(kick);
+        drumSounds.add(snare);
+        drumSounds.add(closedHat);
+        drumSounds.add(openHat);
+    }
+
+    public Sound get(int index) {
+        return drumSounds.get(index);
+    }
+}
---
+++ app/src/main/java/com/example/miniproject/Sound.java
@@ -0,0 +1,15 @@
+package com.example.miniproject;
+
+import android.media.MediaPlayer;
+
+public class Sound {
+    public int resourceID;
+    public int color;
+    public String name;
+
+    public Sound(int resourceID, int color, String name) {
+        this.resourceID = resourceID;
+        this.color = color;
+        this.name = name;
+    }
+}
